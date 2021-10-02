207a87d4f442ff7af7556b31aadc64aaa9c1cf3b
Ai Tran <aicanteven@gmail.com>
2021-10-01 21:55:19 -0700

    setting up health bar tween

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
+[RequireComponent(typeof(RectTransform))]
 public class HealthBar : MonoBehaviour
 {
     public RectTransform health;
-
-    private float rectWidth;
+    //Width of parent rect to determine normalized value of health bar
+    public float rectWidth = 2;
+    public float tweenSpeed = 1;
 
     private float healthBarVal = 0;
     public float HealthBarVal { 
@@ -25,13 +27,17 @@ public class HealthBar : MonoBehaviour
     void Start()
     {
         rectWidth = gameObject.GetComponent<RectTransform>().sizeDelta.x;
-        SetHealthBar(0.5f);
+        SetHealthBar(1);
     }
 
+    /// <param name="val">Normalized value of the health bar</param>
     private void SetHealthBar(float val)
     {
-        DOTween.To(() => healthBarVal, x => healthBarVal = x, 2, 1);
-        health.sizeDelta = new Vector2(healthBarVal, health.sizeDelta.y);
+        float healthSize = Utilities.Remap(val, 0, 1, 2, 0);
+        DOTween.To(() => healthBarVal, x => healthBarVal = x, healthSize, 1).OnUpdate(()=> {
+            //Unity inverts their rect transform when setting the offset max #JustUnityThings
+            health.offsetMax = new Vector2(-healthBarVal, 0);
+        });
     }
 
 }
